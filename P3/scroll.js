97e0e8a641fb2318891ae9f5320920a58bb8c9b1
ninathe <nina.hermanrud@gmail.com>
2018-11-07 14:18:33 +0100

    cleanup code

--- P3/scroll.js
+++ P3/scroll.js
@@ -1,28 +1,28 @@
-// Get a reference to the <path>
-var path = document.querySelector('#star-path');
+//Inspired CSS-tricks-Scroll Drawing: https://css-tricks.com/scroll-drawing/?fbclid=IwAR1aq3enUu3jn1lcvCzbW6bRMoV2iClPHJYRSZSigVNpuf4wtWTygQpfxEI
+
+// Get a reference to the roadsvg
+var road = document.querySelector('#star-path');
 
 // Get length of path... 
-var pathLength = path.getTotalLength();
+var pathLength = road.getTotalLength();
 
 // Jake Archibald says so
 // https://jakearchibald.com/2013/animated-line-drawing-svg/
-path.getBoundingClientRect();
-path.style.transition = path.style.WebkitTransition =
+road.getBoundingClientRect();
+road.style.transition = road.style.WebkitTransition =
 'stroke-dashoffset 0.001s ease-in-out';
 
 // When the page scrolls...
 window.addEventListener("scroll", function(e) {
  
   // What % down is it? 
-  // https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
-  // Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
   var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-  console.log(scrollPercentage);
+  
   //Skjønte ikke en dritt av det her .Skjønte hva greia var, så skrev det sånn at jeg fikk til å jobbe med det //
-  // //document.getElementById("step1").className=(scrollPercentage > 0.01)?"visited":"";
-  // document.getElementById("step2").className=(scrollPercentage > 0.25)?"visited":"";
-  // document.getElementById("step3").className=(scrollPercentage > 0.6)?"visited":"";
-  // document.getElementById("step4").className=(scrollPercentage > 0.95)?"visited":"";
+  document.getElementById("step1").className=(scrollPercentage > 0.01)?"visited":"";
+  document.getElementById("step2").className=(scrollPercentage > 0.27)?"visited":"";
+  document.getElementById("step3").className=(scrollPercentage > 0.62)?"visited":"";
+  document.getElementById("step4").className=(scrollPercentage > 0.95)?"visited":"";
   
 var step1 = document.getElementById("step1");
 var step2 = document.getElementById("step2");
@@ -30,27 +30,27 @@ var step3 = document.getElementById("step3");
 var step4 = document.getElementById("step4");
 
 //Her kan visited.png byttes med screenshots
-if(scrollPercentage > 0.01){
-  erryBodyunvisit();
-  step1.src="visited.png"
-  step1.className = "visited";
-  console.log(step1.className);
-}
-if(scrollPercentage > 0.27){
-  erryBodyunvisit();
-  step2.src="visited.png"
-  step2.className = "visited";
-}
-if(scrollPercentage > 0.62){
-  erryBodyunvisit();
-  step3.src="visited.png"
-  step3.className = "visited";
-}
-if(scrollPercentage > 0.95){
-  erryBodyunvisit();
-  step4.src="visited.png"
-  step4.className = "visited";
-}
+// if(scrollPercentage > 0.01){
+//   erryBodyunvisit();
+//   step1.src="visited.png"
+//   step1.className = "visited";
+//   console.log(step1.className);
+// }
+// if(scrollPercentage > 0.27){
+//   erryBodyunvisit();
+//   step2.src="visited.png"
+//   step2.className = "visited";
+// }
+// if(scrollPercentage > 0.62){
+//   erryBodyunvisit();
+//   step3.src="visited.png"
+//   step3.className = "visited";
+// }
+// if(scrollPercentage > 0.95){
+//   erryBodyunvisit();
+//   step4.src="visited.png"
+//   step4.className = "visited";
+// }
 
 function erryBodyunvisit(){
   step1.src="notVisited.png"
@@ -67,15 +67,15 @@ function erryBodyunvisit(){
   var drawLength = pathLength * scrollPercentage;
   
   // Draw in reverse
-  path.style.strokeDashoffset = pathLength - drawLength;
+  road.style.strokeDashoffset = pathLength - drawLength;
     
   // When complete, remove the dash array, otherwise shape isn't quite sharp
  // Accounts for fuzzy math
   if (scrollPercentage >= 0.99) {
-    path.style.strokeDasharray = 20;
+    road.style.strokeDasharray = 20;
     
   } else {
-    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    road.style.strokeDasharray = pathLength + ' ' + pathLength;
   }
   
 });
