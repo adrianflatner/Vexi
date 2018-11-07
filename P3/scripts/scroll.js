d0b55a3f17ce651c40c9e6605329e36bb842ea0d
ninathe <nina.hermanrud@gmail.com>
2018-11-07 14:46:56 +0100

    changed structure and some refactoring

--- P3/scripts/scroll.js
+++ P3/scripts/scroll.js
@@ -12,56 +12,17 @@ road.getBoundingClientRect();
 road.style.transition = road.style.WebkitTransition =
 'stroke-dashoffset 0.001s ease-in-out';
 
-// When the page scrolls...
+// When the page scrolls
 window.addEventListener("scroll", function(e) {
  
-  // What % down is it? 
+  // What % of the page is scrolled? 
   var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
   
-  //Skjønte ikke en dritt av det her .Skjønte hva greia var, så skrev det sånn at jeg fikk til å jobbe med det //
+  //When scrolling passed, the image will get the class visited
   document.getElementById("step1").className=(scrollPercentage > 0.01)?"visited":"";
   document.getElementById("step2").className=(scrollPercentage > 0.27)?"visited":"";
   document.getElementById("step3").className=(scrollPercentage > 0.62)?"visited":"";
   document.getElementById("step4").className=(scrollPercentage > 0.95)?"visited":"";
-  
-var step1 = document.getElementById("step1");
-var step2 = document.getElementById("step2");
-var step3 = document.getElementById("step3");
-var step4 = document.getElementById("step4");
-
-//Her kan visited.png byttes med screenshots
-// if(scrollPercentage > 0.01){
-//   erryBodyunvisit();
-//   step1.src="visited.png"
-//   step1.className = "visited";
-//   console.log(step1.className);
-// }
-// if(scrollPercentage > 0.27){
-//   erryBodyunvisit();
-//   step2.src="visited.png"
-//   step2.className = "visited";
-// }
-// if(scrollPercentage > 0.62){
-//   erryBodyunvisit();
-//   step3.src="visited.png"
-//   step3.className = "visited";
-// }
-// if(scrollPercentage > 0.95){
-//   erryBodyunvisit();
-//   step4.src="visited.png"
-//   step4.className = "visited";
-// }
-
-function erryBodyunvisit(){
-  step1.src="notVisited.png"
-  step1.className = "unvisited";
-  step2.src="notVisited.png"
-  step2.className = "unvisited";
-  step3.src="notVisited.png"
-  step3.className = "unvisited";
-  step4.src="notVisited.png"
-  step4.className = "unvisited";
-}
 
   // Length to offset the dashes
   var drawLength = pathLength * scrollPercentage;
@@ -70,10 +31,8 @@ function erryBodyunvisit(){
   road.style.strokeDashoffset = pathLength - drawLength;
     
   // When complete, remove the dash array, otherwise shape isn't quite sharp
- // Accounts for fuzzy math
   if (scrollPercentage >= 0.99) {
-    road.style.strokeDasharray = 20;
-    
+    road.style.strokeDasharray = 20; 
   } else {
     road.style.strokeDasharray = pathLength + ' ' + pathLength;
   }
